178447ae2f14fb7c785c53f5c1887510904ea983
Cavid <hcavid386@gmail.com>
2021-12-03 00:24:39 +0400

    0:24

--- Script.js
+++ Script.js
@@ -1,18 +1,5 @@
-/*
-arr=[4,5,6,45,6,5,8,9,4,36,1,4,5,8,65,54,21,2]
 
-
-Array.prototype.MylastIndexOf = function(element){
-    for(let i = this.length - 1; i >= 0; i--){
-        if(this[i] === element){
-            return i;
-        };
-    };
-    return -1;
-};
-console.log(arr.MylastIndexOf(4));
-*/
-arry=[4,5,6]
+arry=[4,5,6,45,6,5,8,9,4,36,1,4,5,8,65,54,21,2]
 
 
 Array.prototype.MyIndexOf=function(element){
