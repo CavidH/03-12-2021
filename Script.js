3e8376b990b7460cddab5c39f4a9be66ae8b430d
Cavid <hcavid386@gmail.com>
2021-12-03 00:20:53 +0400

    First task

---
+++ Script.js
@@ -0,0 +1,27 @@
+/*
+arr=[4,5,6,45,6,5,8,9,4,36,1,4,5,8,65,54,21,2]
+
+
+Array.prototype.MylastIndexOf = function(element){
+    for(let i = this.length - 1; i >= 0; i--){
+        if(this[i] === element){
+            return i;
+        };
+    };
+    return -1;
+};
+console.log(arr.MylastIndexOf(4));
+*/
+arry=[4,5,6]
+
+
+Array.prototype.MyIndexOf=function(element){
+    for(let i = 0; i<this.length;i++){
+        if(this[i]==element){
+            return i;
+        }
+    }
+    return -1;
+};
+
+console.log(arry.MyIndexOf(6))
